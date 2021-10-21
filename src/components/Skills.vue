f95ad9a8b5105aeb9dd9ccc795341a09af6a647d
Bayardo <bjlslopez@gmail.com>
2021-10-21 13:40:27 -0600

    Using directives v-if and v-for

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">{{index}}. {{data.skill}}</li>
+      </ul>
+
+      <p v-if="skills.length >= 1">You have more than 1 skills</p>
+      <p v-else>You have 0 skills</p>
+    </div>
     {{ name }}
 
-    {{ btnState ? 'The button is disabled' : 'The button is active' }}
+    {{ btnState ? "The button is disabled" : "The button is active" }}
 
-    <button v-on:click="changeName" v-bind:disabled="btnState">Change Name</button>
+    <button v-on:click="changeName" v-bind:disabled="btnState">
+      Change Name
+    </button>
   </div>
 </template>
 
@@ -15,7 +25,10 @@ export default {
     return {
       name: "Coursetro",
       btnState: true,
-
+      skills: [
+        { skill: "Vue.js" },
+        { skill: "Frontend Developer" },
+        ],
     };
   },
 };
