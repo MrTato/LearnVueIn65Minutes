0c94b3fcdafb6ee8b75e7909e11120dc19fffefd
Bayardo <bjlslopez@gmail.com>
2021-10-21 15:03:34 -0600

    Two way data binding configured

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="hello">
     <div class="holder">
+      <input type="text" placeholder="Enter a skill..." v-model="skill" />
+      {{ skill }}
+
       <ul>
         <li v-for="(data, index) in skills" :key="index">
-          {{ index }}. {{ data.skill }}
+          {{ data.skill }}
         </li>
       </ul>
 
-      <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }"></div>
+      <p>These are the skills that you possess.</p>
     </div>
   </div>
 </template>
@@ -17,10 +20,8 @@ export default {
   name: "Skills",
   data() {
     return {
+      skill: '',
       skills: [{ skill: "Vue.js" }, { skill: "Frontend Developer" }],
-      bgColor: 'yellow',
-      bgWidth: '100%',
-      bgHeight: '30px'
     };
   },
 };
@@ -28,13 +29,61 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .alert {
-    background-color: yellow;
-    width: 100%;
-    height: 30px;
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+  body {
+    background-color: #EEEEEE;
+    font-family: 'Montserrat', 'sans-serif';
+    display: grid;
+    grid-template-rows: auto;
+    justify-items: center;
+    align-items: center;
   }
 
-  .another-class {
-    border: 5px solid black;
+  body, html {
+    margin: 0;
+    height: 100%;
+  }
+
+  #app {
+    width: 50%;
+  }
+
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align: center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
   }
 </style>
